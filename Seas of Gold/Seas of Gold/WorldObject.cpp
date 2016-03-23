bd5093bf51f1a7eebc5970704d10decea180e142
Omni Platinum Games <main.ocpgc@gmail.com>
2016-03-22 23:20:35 -0500

    Fixed Typedef Ambiguity
    Specifically vector3d -> v3d and string -> irrString

--- Seas of Gold/Seas of Gold/WorldObject.cpp
+++ Seas of Gold/Seas of Gold/WorldObject.cpp
@@ -1,10 +1,10 @@
 #include "WorldObject.h"
 
-WorldObject::WorldObject(vector3d* l, vector3d* r, vector3d* s, string n, int m, bool c, bool v, long* GUIDList[], int len){
-	m_v3Loc = vector3d(*l);
-	m_v3Rot = vector3d(*r);
-	m_v3Scale = vector3d(*s);
-	m_isName = string(n);
+WorldObject::WorldObject(v3d* l, v3d* r, v3d* s, irrString n, int m, bool c, bool v, long* GUIDList[], int len){
+	m_v3Loc = v3d(*l);
+	m_v3Rot = v3d(*r);
+	m_v3Scale = v3d(*s);
+	m_isName = irrString(n);
 	m_iModelID = m;
 	m_bClipping = c;
 	m_bVisible = v;
