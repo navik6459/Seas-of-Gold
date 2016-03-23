8ab099df4dc3284cdac2040d4d14b87cb579f06b
Bly7 <roaming111@aol.com>
2016-03-23 16:27:29 -0700

    Added Mission.h and Common.h
    Mission.h: Base mission header and class.
    
    Common.h: common header that contains global code.

---
+++ Seas of Gold/Seas of Gold/Common.h
@@ -0,0 +1,9 @@
+// Common.h
+//
+// Holds Things that will be used throughout the program
+
+#include "vector3d.h"
+#include "irrString.h"
+
+typedef irr::core::vector3d<float> v3d;
+typedef irr::core::string<char> irrstring;
---
+++ Seas of Gold/Seas of Gold/Mission.cpp
@@ -0,0 +1,64 @@
+#include "Mission.h"
+
+Mission::Mission()
+{
+	m_1ID = 0;
+	irrstring m_isDisplayName = "Default Mission";
+	irrstring m_isGoalText = "Default Mission Goal Text!";
+	// m_itRequired = ?
+	// m_itReward = ?
+	m_iGoldReward = 0;
+	m_1TurnInGUID = 0;
+}
+
+/*
+Mission::Mission(long ID,  irrstring DispName, irrstring GoalText, Items* isReq, Items* isRew, int GoldRew);
+{
+	m_1ID = ID;
+	irrstring m_isDisplayName = DispName;
+	irrstring m_isGoalText = GoalText;
+	// m_itRequired = isReq;
+	// m_itReward = isRew;
+	m_iGoldReward = GoldRew;
+	m_1TurnInGUID = ? // However we generate this
+}
+*/
+
+long Mission::getMissionID()
+{
+	return m_1ID;
+}
+
+irrstring Mission::getDisplayName()
+{
+	return m_isDisplayName;
+}
+
+irrstring Mission::getGoalText()
+{
+	return m_isGoalText;
+}
+
+/*
+Item* Mission::getRequiredItems()
+{
+	return m_itRequired;
+}
+*/
+
+/*
+Item* Mission::getRewardItems()
+{
+	return m_itReward;
+}
+*/
+
+int Mission::getRewardGold()
+{
+	return m_iGoldReward;
+}
+
+long Mission::getTurnInGUID()
+{
+	return m_1TurnInGUID;
+}
