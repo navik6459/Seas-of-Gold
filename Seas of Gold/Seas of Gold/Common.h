55c22b63e019d25028a18c0176c79f0cc412315a
Omni Platinum Games <main.ocpgc@gmail.com>
2016-03-23 23:33:15 -0500

    Revert "Added Inventory Typedef"
    This reverts commit 638939bb7323c2589bebce913a92ef75f6fdf7f5.

--- Seas of Gold/Seas of Gold/Common.h
+++ Seas of Gold/Seas of Gold/Common.h
@@ -4,8 +4,6 @@
 
 #include "vector3d.h"
 #include "irrString.h"
-#include <map>
 
 typedef irr::core::vector3d<float> v3d;
 typedef irr::core::string<char> irrstring;
-typedef std::map<int, int> inventory;
