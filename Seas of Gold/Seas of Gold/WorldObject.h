cacac9dce7df72246f65ce18dba0515d666dca5e
Allen Underwood <aunder6235@gmail.com>
2016-03-23 20:11:17 -0700

    Unit class
    Unfinished, but mostly done.

--- Seas of Gold/Seas of Gold/WorldObject.h
+++ Seas of Gold/Seas of Gold/WorldObject.h
@@ -1,15 +1,15 @@
 #include "vector3d.h"
 #include "irrString.h"
 
-typedef irr::core::vector3d<float> vector3d;
-typedef irr::core::string<char> string;
+typedef irr::core::vector3d<float> v3d;
+typedef irr::core::string<char> irrString;
 
 class WorldObject{
-	vector3d m_v3Loc;
-	vector3d m_v3Rot;
-	vector3d m_v3Scale;
+	v3d m_v3Loc;
+	v3d m_v3Rot;
+	v3d m_v3Scale;
 	long m_lGUID;
-	string m_isName;
+	irrString m_isName;
 	int m_iModelID;
 	bool m_bClipping;
 	bool m_bVisible;
@@ -37,22 +37,22 @@ class WorldObject{
 	}
 
 public:
-	inline vector3d* getLocation() { return &m_v3Loc; };
-	inline void setLocation(vector3d newLoc) { m_v3Loc = vector3d(newLoc); };
-	inline void setLocation(float x, float y, float z){ m_v3Loc = vector3d(x, y, z); };
-	inline vector3d* getRotation() { return &m_v3Rot; };
-	inline void setRotation(vector3d newRot){ m_v3Rot = vector3d(newRot); };
-	inline void setRotation(float x, float y, float z){ m_v3Rot = vector3d(x, y, z); };
-	inline vector3d* getScale() { return &m_v3Scale; };
-	inline void setScale(vector3d newScale){ m_v3Scale = vector3d(newScale); };
-	inline void setScale(float x, float y, float z){ m_v3Scale = vector3d(x, y, z); };
+	inline v3d* getLocation() { return &m_v3Loc; };
+	inline void setLocation(v3d newLoc) { m_v3Loc = v3d(newLoc); };
+	inline void setLocation(float x, float y, float z){ m_v3Loc = v3d(x, y, z); };
+	inline v3d* getRotation() { return &m_v3Rot; };
+	inline void setRotation(v3d newRot){ m_v3Rot = v3d(newRot); };
+	inline void setRotation(float x, float y, float z){ m_v3Rot = v3d(x, y, z); };
+	inline v3d* getScale() { return &m_v3Scale; };
+	inline void setScale(v3d newScale){ m_v3Scale = v3d(newScale); };
+	inline void setScale(float x, float y, float z){ m_v3Scale = v3d(x, y, z); };
 	inline long getGUID(){ return m_lGUID; };
-	inline string getName(){ return m_isName; };
+	inline irrString getName(){ return m_isName; };
 	inline int getModelById(){ return m_iModelID; };
 	inline bool hasClipping(){ return m_bClipping; };
 	inline bool isVisible(){ return m_bVisible; };
 	inline long getOwner(){ return m_lOwner; };
 	inline void setOwner(long ownGUID){ m_lOwner = ownGUID; };
 
-	WorldObject(vector3d* l, vector3d* r, vector3d* s, string n, int m, bool c, bool v, long* GUIDList[], int len);
+	WorldObject(v3d* l, v3d* r, v3d* s, irrString n, int m, bool c, bool v, long* GUIDList[], int len);
 };
